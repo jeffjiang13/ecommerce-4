5be3bc3c2f49b737ed19ccb9c145cec501cdbf25
Jeff Jiang <jeff.jiang13@gmail.com>
2023-05-04 17:41:16 -0400

    first commit

---
+++ frontend/src/services/AuthServices.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const Register = async (firstName, lastName, email, password, phone) =>{
+    console.log('Calling Signup with', { firstName, lastName, email, password, phone }); // Add this line
+
+    return await axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/register`,{
+        firstName,
+        lastName,
+        email,
+        password,
+        phone
+    });
+};
+
+export const Login = async (email, password)=>{
+    return await axios.post(`${process.env.REACT_APP_API_BASE_URL}/users/login`,{
+        email,
+        password
+    });
+};
